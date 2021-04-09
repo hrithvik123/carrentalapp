8a5a6f76be620fa6cf5483356d4352573c60d990
hrithvik123 <gadhiyahrithvik@gmail.com>
2021-04-10 01:19:49 +0530

    Edited models, made change to login, logout, register

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from . import views as users_views
+from allauth.account.views import LoginView, LogoutView, PasswordResetView, SignupView
+
 
 urlpatterns = [
     path('', users_views.home, name='home'),
+    path('register/', SignupView.as_view(template_name='users/register.html'), name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_reset/', PasswordResetView.as_view(
+         template_name='users/password_reset.html'), name='reset_password'),
     path('contact_us/', users_views.contactUs, name='contact-us'),
     path('featured/', users_views.featuredCars, name='featured'),
     path('testdrive/', users_views.testDrive, name='test-drive'),
