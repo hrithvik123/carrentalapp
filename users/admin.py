dbe6106fc2e022eca8d92ae77d0e111b433e08b6
hrithvik123 <gadhiyahrithvik@gmail.com>
2021-04-09 23:31:44 +0530

    Fixed Models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import (
+    User, Customer, Sales_Associate, Manager,
+    Rental_Package, Vehicle, Booking, Insurance
+)
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Customer)
+admin.site.register(Sales_Associate)
+admin.site.register(Manager)
+admin.site.register(Rental_Package)
+admin.site.register(Vehicle)
+admin.site.register(Booking)
+admin.site.register(Insurance)
--- users/models.py
+++ users/models.py
@@ -1,106 +1,116 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from datetime import datetime, timedelta
 
 
 # Create your models here.
 
+class User(AbstractUser):
+    gender_choices = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    gender = models.CharField(max_length=1, choices=gender_choices, blank=True)
+    age = models.IntegerField(max_length=2, default='21', blank=True)
+    contactNo = models.PositiveIntegerField(
+        max_length=10, default='0', blank=True)
+
+
 class Login(models.Model):
     email_id = models.OneToOneField(
         User, on_delete=models.CASCADE, unique=True)
-    password =   models.CharField(
+    password = models.CharField(
         max_length=255, blank=False, default="Login")
-    
-     def __str__(self):
-        return self.name
-        
-class Customer(models.Model):
-    name = models.CharField(
-        max_length=255, blank=False, default="Customer")
-    gender= models.CharField(
-        max_length=255, blank=False, default="Customer")
-    age = models.IntegerField(default=18)
-    phone_number = models.CharField(
-        max_length=255, blank=False, default="Customer")
-    drivers_license =  models.IntegerField(default=0, unique = True)
-    email_id =  models.ForeignKey(Login, on_delete=models.CASCADE)
-    
-    
-class Manager(model.Model):
-    name = models.CharField(
-        max_length=255, blank=False, default="Customer")
-    gender= models.CharField(
-        max_length=255, blank=False, default="Customer")
-    age = models.IntegerField(default=18)
-    phone_number = models.CharField(
-        max_length=255, blank=False, default="Customer")
-    manager_ssn =  models.IntegerField(default=0, unique = True)
-    email_id =  models.ForeignKey(Login, on_delete=models.CASCADE)
-    
-    
-class Sales_Associate(model.Model):
-    name = models.CharField(
-        max_length=255, blank=False, default="Customer")
-    gender= models.CharField(
-        max_length=255, blank=False, default="Customer")
-    age = models.IntegerField(default=18)
-    phone_number = models.CharField(
-        max_length=255, blank=False, default="Customer")
-    Ssn =  models.IntegerField(default=0, unique = True)
-    email_id =  models.ForeignKey(Login, on_delete=models.CASCADE)
-    
-    
-class Rental_Package(model.Model):
+
+    # def __str__(self):
+    #     return self.name
+
+
+class Customer(User):
+    drivers_license = models.IntegerField(default=0, unique=True)
+
+    class Meta:
+        # db_table = 'customer'
+        # Add verbose name
+        verbose_name = 'Customer'
+
+
+class Sales_Associate(User):
+    Ssn = models.IntegerField(default=0, unique=True)
+
+    class Meta:
+        # db_table = 'sales_associate'
+        # Add verbose name
+        verbose_name = 'Sales Associate'
+
+
+class Manager(User):
+    manager_ssn = models.IntegerField(default=0, unique=True)
+
+    class Meta:
+        # db_table = 'manager'
+        # Add verbose name
+        verbose_name = 'Manager'
+
+
+class Rental_Package(models.Model):
     per_day_rent = models.IntegerField(default=0)
     per_month_rent = models.IntegerField(default=0)
-    touring_package= models.IntegerField(default=0)
+    touring_package = models.IntegerField(default=0)
 
-class Booking(model.Model):
-    start_time = models.DateTimeField(default=timezone.now)
+
+class Booking(models.Model):
+    # default start of booking is next day
+    start_time = models.DateTimeField(
+        default=(datetime.now()+timedelta(days=1)))
     end_time = models.DateTimeField(
-        default=(datetime.now()+timedelta(minutes=5)))
+        default=(datetime.now()+timedelta(hours=48)))  # default end time is 24hrs from booking.
     package_id = models.ForeignKey(Rental_Package, on_delete=models.CASCADE)
     sales_id = models.ForeignKey(Sales_Associate, on_delete=models.CASCADE)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    
-class Customer_Service(model.Model):
-    rate = models.IntegerField(default = 5)
+
+
+class Customer_Service(models.Model):
+    rate = models.IntegerField(default=5)
     feedback = models.CharField(max_length=255, blank=False)
     bot_chat = models.BooleanField()
     customer_question = models.CharField(max_length=255, blank=False)
     sales_id = models.ForeignKey(Sales_Associate, on_delete=models.CASCADE)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    
-class Vehicle(model.Model):
+
+
+class Vehicle(models.Model):
+    transmission_choices = (
+        ('A', 'Automatic'),
+        ('M', 'Manual')
+    )
+    engine_no = models.CharField(max_length=20, blank=False, default=None)
     price = models.IntegerField()
     make = models.CharField(max_length=255, blank=False)
-    model =  models.CharField(max_length=255, blank=False)
+    model = models.CharField(max_length=255, blank=False)
     seating_cap = models.IntegerField()
-    transmission = models.ChoiceField("Automatic","Manual")
+    transmission = models.CharField(max_length=1, choices=transmission_choices)
     availability = models.BooleanField()
-    
-    
-class Insurance(model.Model):
+
+
+class Insurance(models.Model):
     vehicle_id = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     insurance_prov = models.CharField(max_length=255, blank=False)
     price = models.IntegerField()
-    
 
-class Manager_creates_pack(model.Model):
-    manager_id = models.ForeignKey(Manager, on_delete = models.CASCADE)
-    package_id = models.ForeignKey(Rental_Package, on_delete = models.CASCADE)
-    
-class Customer_testdrive(model.Model):
+
+# maybe we don't need this, we should manager creates rental package as M:N
+# but it should actually be 1:N
+class Manager_creates_pack(models.Model):
+    manager_id = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    package_id = models.ForeignKey(Rental_Package, on_delete=models.CASCADE)
+
+
+class Customer_testdrive(models.Model):
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     vehicle_id = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     route = models.CharField(max_length=255, blank=False)
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(
         default=(datetime.now()+timedelta(minutes=5)))
-    
-    
-    
-    
-    
- 
