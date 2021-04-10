9d084ebbe8f05f98b200e300dba42bb8b61e7560
hrithvik123 <gadhiyahrithvik@gmail.com>
2021-04-10 07:26:32 +0530

    Implemented Creating rentals, listing cars, customer bookings, modifying bookings

---
+++ users/signals.py
@@ -0,0 +1,104 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import User, Customer, Booking, Rental_Package
+from django.utils import timezone
+from datetime import datetime
+
+
+# if User is created, automatically create a customer instance
+@receiver(post_save, sender=User)
+def create_customer(sender, instance, created, **kwargs):
+    if created:
+        cust = instance
+        cust.__class__ = Customer
+        cust.drivers_license = 0
+        cust.save(force_insert=True)
+
+
+# if Booking is created or changed, update price if applicable
+# use temp_amount to avoid an infinite loop
+@receiver(post_save, sender=Booking)
+def apply_booking(sender, instance, created, **kwargs):
+    if created:
+        instance.vehicle.availability = False
+        instance.vehicle.save()
+        duration_seconds = instance.end_time - instance.start_time
+        duration = duration_seconds.total_seconds()
+        duration = duration/(60*60*24)
+
+        if duration < 15:
+            temp_amount = instance.vehicle.package.per_day_rent*duration
+            if instance.amount != temp_amount:
+                instance.amount = temp_amount
+                instance.save()
+            return
+        if duration >= 15 and duration < 30:
+            temp_amount = instance.vehicle.package.touring_package * \
+                (duration/15)
+            if instance.amount != temp_amount:
+                instance.amount = temp_amount
+                instance.save()
+            return
+        if duration >= 30:
+            temp_amount = instance.vehicle.package.per_month_rent*(duration/30)
+            if instance.amount != temp_amount:
+                instance.amount = temp_amount
+                instance.save()
+            return
+
+    elif not created:
+        # start = datetime.combine(datetime.now(), instance.start_time)
+        # end = datetime.combine(datetime.now(), instance.end_time)
+        duration_seconds = instance.end_time - instance.start_time
+        duration = duration_seconds.total_seconds()
+        duration = duration/(60*60*24)
+
+        if duration < 15:
+            temp_amount = instance.vehicle.package.per_day_rent*duration
+            if instance.amount != temp_amount:
+                instance.amount = temp_amount
+                instance.save()
+            return
+        if duration >= 15 and duration < 30:
+            temp_amount = instance.vehicle.package.touring_package * \
+                (duration/15)
+            if instance.amount != temp_amount:
+                instance.amount = temp_amount
+                instance.save()
+            return
+        if duration >= 30:
+            temp_amount = instance.vehicle.package.per_month_rent*(duration/30)
+            if instance.amount != temp_amount:
+                instance.amount = temp_amount
+                instance.save()
+            return
+
+
+# implement automatic updation of booking prices if rental package prices change
+# incomplete
+@receiver(post_save, sender=Rental_Package)
+def update_amount(sender, instance, created, **kwargs):
+    for book in Booking.objects.filter(vehicle.package == instance):
+        duration_seconds = book.end_time - book.start_time
+        duration = duration_seconds.total_seconds()
+        duration = duration/(60*60*24)
+
+        if duration < 15:
+            temp_amount = instance.package.per_day_rent*duration
+            if book.amount != temp_amount:
+                book.amount = temp_amount
+                book.save()
+            return
+        if duration >= 15 and duration < 30:
+            temp_amount = instance.package.touring_package * \
+                (duration/15)
+            if book.amount != temp_amount:
+                book.amount = temp_amount
+                book.save()
+            return
+        if duration >= 30:
+            temp_amount = instance.package.per_month_rent*(duration/30)
+            if book.amount != temp_amount:
+                book.amount = temp_amount
+                book.save()
+            return
--- users/views.py
+++ users/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib import messages
+from django.views.generic import (
+    ListView,
+    DetailView,  # import classbased views
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Vehicle, Booking, Rental_Package, User
 # Create your views here.
 
 
@@ -7,8 +16,197 @@ def home(request):
     return render(request, 'users/index.html')
 
 
-def featuredCars(request):
-    return render(request, 'users/featured_cars.html')
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ['first_name', 'last_name', 'email', 'gender', 'age', 'contactNo']
+    template_name = 'users/profile.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Profile updated successfully')
+        return super().form_valid(form)
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('home')
+
+
+# View for List of cars available to customer
+class UserVehicleListView(LoginRequiredMixin, ListView):
+    model = Vehicle
+    template_name = 'users/vehicle_all.html'
+    context_object_name = 'vehicles'
+
+    def get_queryset(self):
+        return Vehicle.objects.filter(availability=True)
+
+
+# View for all vehicles in database, for admin
+class AdminVehicleListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Vehicle
+    template_name = 'staff/vehicle_all.html'
+    context_object_name = 'vehicles'
+
+    # check if user is admin
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+
+# View for admin to update car's availability
+class AdminVehicleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Vehicle
+    fields = ['availability']
+    template_name = 'staff/vehicle_edit.html'
+
+    # check if user is admin
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('admin-vehicle-all')
+
+
+# view for admin to create vehicle
+class VehicleCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Vehicle
+    fields = ['make', 'model', 'engine_no', 'seating_cap',
+              'transmission', 'package', 'availability']
+    template_name = 'staff/vehicle_new.html'
+
+    # check if form is valid
+    def form_valid(self, form):
+        form.instance.price = 10000
+        messages.success(self.request, f'Car Added')
+        return super().form_valid(form)
+
+    # check if user is admin
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('vehicle-all')
+
+
+# View for List of rental packages for admin
+class RentalListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Rental_Package
+    template_name = 'staff/rental_all.html'
+    context_object_name = 'rentals'
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+# View for admin to update the Rental packages
+
+
+class RentalUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Rental_Package
+    fields = ['per_day_rent', 'per_month_rent', 'touring_package']
+    template_name = 'staff/rental_edit.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Rental package updated successfully')
+        return super().form_valid(form)
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('rental-all')
+
+
+# View for admin to create new rental packages
+class RentalCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Rental_Package
+    fields = ['type', 'per_day_rent', 'per_month_rent', 'touring_package']
+    template_name = 'staff/rental_new.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Rental Added')
+        return super().form_valid(form)
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('rental-all')
+
+
+# View for customer to make a booking
+class BookingCreateView(LoginRequiredMixin, CreateView):
+    model = Booking
+    fields = ['vehicle', 'start_time', 'end_time']
+    template_name = 'users/booking_new.html'
+
+    def form_valid(self, form):
+        form.instance.customer_id = self.request.user.customer
+        form.instance.sales_id = None
+        messages.success(self.request, 'Booking made successfully')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('booking-all')
+
+
+# View for User to see his/her bookings
+class UserBookingListView(LoginRequiredMixin, ListView):
+    model = Booking
+    context_object_name = 'bookings'
+    template_name = 'users/booking_all.html'
+
+    def get_queryset(self):
+        return Booking.objects.filter(customer_id=self.request.user.customer)
+    # def test_func(self):
+    #     if self.request.user.is_superuser:
+    #         return True
+    #     return False
+
+
+# View for Admin to see all bookings
+class AdminBookingListView(LoginRequiredMixin, ListView):
+    model = Booking
+    context_object_name = 'bookings'
+    template_name = 'staff/booking_all.html'
+
+    # def test_func(self):
+    #     if self.request.user.is_superuser:
+    #         return True
+    #     return False
+
+
+# View for admin to edit a booking
+class BookingUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Booking
+    fields = ['start_time', 'end_time']
+    template_name = 'staff/booking_edit.html'
+
+    def form_valid(self, form):
+        form.instance.sales_id = None
+        messages.success(self.request, f'Booking updated successfully')
+        return super().form_valid(form)
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('admin-booking-all')
 
 
 def contactUs(request):
